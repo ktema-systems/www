939b8321f66282c6fbb9e1443cf9621eedeab01c
TH Lee <lee@ssccs.org>
2026-09-26 22:19:22 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ export const papers = [
     href: 'https://doi.org/10.5281/zenodo.18759106',
   },
   {
-    title: 'Tagma: Hashless spatial primitive on a fixed 16-bit, 3-axis Unicode composition space',
+    title: 'Tagma: Hashless coordinate primitive in the fixed 16-bit Unicode composition block',
     when: 'July 2026',
     href: 'https://doi.org/10.5281/zenodo.21302508',
   },
